d13aeaf1cd75361ea20a629d943bf91b8a1ce0f2
Mats Fjeldstad <mats.fjeldstad@outlook.com>
2023-01-03 15:53:51 +0100

    #4 adds tailwind with postCss

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css';
 import React from 'react';
 
 function App() {
-  return <div className="App" />;
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
 export default App;
