3a602217e764edb56fca4175538a2f1174c76fde
Mats Fjeldstad <mats.fjeldstad@outlook.com>
2023-01-03 14:44:54 +0100

    #3 adds eslint and husky

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div className="App"></div>;
+  return <div className="App" />;
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
+npm install husky --save-dev 
+npx husky-init                 npm install lint-staged --save-dev   
