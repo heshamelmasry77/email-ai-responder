fad7c8c7f410eedc4c54997d0d7bcf7325f7360c
Mats Fjeldstad <mats.fjeldstad@outlook.com>
2023-01-05 14:08:18 +0100

    adds a basic nodeJS server to call openAI API

---
+++ script.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import cors from 'cors';
+import { Configuration, OpenAIApi } from 'openai';
+
+const app = express();
+app.use(cors());
+
+const configuration = new Configuration({
+  apiKey: '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',
+});
+const openai = new OpenAIApi(configuration);
+
+const completion = await openai.createCompletion({
+  model: 'text-davinci-003',
+  prompt: 'if the api is working, just write "test is ok" ',
+  temperature: 0.6,
+  max_tokens: 10,
+});
+const response = completion.data.choices[0].text;
+
+app.get('/', (req, res) => {
+  res.send(response);
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
