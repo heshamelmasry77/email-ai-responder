ee7b55d785076e52bb48b20e32d63753ce5efa47
hesh ramsis <heshamelmasry1992@gmail.com>
2023-01-12 16:38:28 +0100

    allow origins

--- server/index.js
+++ server/index.js
@@ -12,9 +12,29 @@ const __dirname = path.resolve();
 
 const app = express();
 app.use(bodyParser.json());
-app.use(cors({
-  origin: '*',
-}));
+
+const allowedOrigins = [
+  'http://localhost:3001',
+  'http://127.0.0.1:5173',
+  'https://email-ai-responder-4ncopa7xi-heshamelmasry77.vercel.app',
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // allow requests with no origin
+      // (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        let msg =
+          "The CORS policy for this site does not " +
+          "allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 
 const PORT = 3001;
 
